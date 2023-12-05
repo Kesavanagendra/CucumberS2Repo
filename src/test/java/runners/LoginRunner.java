dfce85630f57af66f887751647e43d0ccc06442a
Kesavanagendra <140937702+Kesavanagendra@users.noreply.github.com>
2023-12-05 10:44:19 +0530

    Update LoginRunner.java

--- src/test/java/runners/LoginRunner.java
+++ src/test/java/runners/LoginRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\java\\features\\Login.feature",
+@CucumberOptions(features = "src\test\java\features\Login.feature",
 					glue = {"steps"},
 					dryRun = false,
 					plugin = {"pretty","html:target/Report1.html"}
