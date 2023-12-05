d19d8a03bc83a2d057832881564fc44fc3a100d6
Kesavanagendra <140937702+Kesavanagendra@users.noreply.github.com>
2023-12-05 12:51:29 +0530

    Update WebshopHooks.java

--- src/test/java/steps/WebshopHooks.java
+++ src/test/java/steps/WebshopHooks.java
@@ -22,6 +22,10 @@ options.addArguments("--disable-extensions"); // disabling extensions
 options.addArguments("--disable-gpu"); // applicable to windows os only
 options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 options.addArguments("--no-sandbox"); // Bypass OS security model
+
+		
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
 		
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
